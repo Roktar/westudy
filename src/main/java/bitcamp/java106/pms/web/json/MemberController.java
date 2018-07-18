9ce0674e4aeb2d4c14180b8dc926e1d3613b7dd3
Roktar <outofsight_@naver.com>
2018-07-18 20:04:50 +0900

    .

--- src/main/java/bitcamp/java106/pms/web/json/MemberController.java
+++ src/main/java/bitcamp/java106/pms/web/json/MemberController.java
@@ -1,53 +1,56 @@
-package main.java.bitcamp.java106.pms.web.json;
+package bitcamp.java106.pms.web.json;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.MatrixVariable;
-import org.springframework.web.bind.annotation.PathVariable;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-import main.java.bitcamp.java106.pms.domain.Member;
-import main.java.bitcamp.java106.pms.service.MemberService;
 
-@RestController
+import bitcamp.java106.pms.domain.Member;
+import bitcamp.java106.pms.service.MemberService;
+
+@Controller
 @RequestMapping("/member")
 public class MemberController {
-    
     MemberService memberService;
     
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
-
+    
     @RequestMapping("add")
-    @ResponseStatus(HttpStatus.CREATED)
-    public void add(Member member) throws Exception {
-        memberService.add(member);
+    public void insert(@RequestBody String qs) {
+        String[] seperateParameter = qs.split("&");
+        Map<String, String> seperateKeyValue = new HashMap<>();
+        
+        System.out.println(qs);
+        
+        try {
+            for(String param : seperateParameter) {
+                String[] keyVal = param.split("=");
+                keyVal[0] = URLDecoder.decode(keyVal[0], "UTF-8");
+                keyVal[1] = URLDecoder.decode(keyVal[1], "UTF-8");
+                
+/*                if(keyVal[0].startsWith("interests")) 
+                    keyVal[0] = keyVal[0].substring(12) + keyVal[0].substring(10, 11);*/
+                
+                seperateKeyValue.put(keyVal[0], keyVal[1]);
+            }
+            
+            System.out.println(seperateKeyValue);
+            
+        } catch (UnsupportedEncodingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
     }
     
-    @RequestMapping("delete")
-    //@ResponseStatus(HttpStatus.OK) // 응답 상태 코드 값의 기본은 "200(OK)" 이다.
-    public void delete(@RequestParam("no") int no) throws Exception {
-        memberService.delete(no);
+    @RequestMapping("{id}")
+    public Member selectOne(String id) {
+        return memberService.selectOne(id);
     }
-    
-    @RequestMapping("list{page}")
-    public Object list(
-            @MatrixVariable(defaultValue="1") int pageNo,
-            @MatrixVariable(defaultValue="3") int pageSize) {        
-        return memberService.list(pageNo, pageSize);
-    }
-    
-    @RequestMapping("update")
-    @ResponseStatus(HttpStatus.OK) // 기본 값이 OK 이다. 
-    public void update(Member member) throws Exception {
-        memberService.update(member);
-    }
-    
-    @RequestMapping("{no}")
-    public Member view(@PathVariable int no) throws Exception {
-        return memberService.get(no);
-    }
-
 }
