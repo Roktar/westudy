8c11eef5b90ad05faf47e69cb82e540a8bbbb4d1
Boram Choi <boram3510@gmail.com>
2018-07-20 11:02:48 +0900

    .

--- src/main/java/bitcamp/java106/pms/web/json/MemberController.java
+++ src/main/java/bitcamp/java106/pms/web/json/MemberController.java
@@ -1,17 +1,25 @@
 package bitcamp.java106.pms.web.json;
 
+import java.io.File;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import bitcamp.java106.pms.domain.Member;
 import bitcamp.java106.pms.service.MemberService;
+import net.coobird.thumbnailator.Thumbnails;
 
 @RestController
 @RequestMapping("/member")
@@ -55,4 +63,38 @@ public class MemberController {
     public Member selectOne(String id) {
         return memberService.selectOne(id);
     }
+    
+    @Autowired ServletContext sc;
+
+    
+    @PostMapping("upload04")
+    public Object upload04(
+            MultipartFile files) {
+        
+        HashMap<String,Object> jsonData = new HashMap<>();
+        
+        String filesDir = sc.getRealPath("/files");
+        
+        String filename = UUID.randomUUID().toString();
+        jsonData.put("filename", filename);
+        jsonData.put("filesize", files.getSize());
+        jsonData.put("originname", files.getOriginalFilename());
+        try {
+            File path = new File(filesDir + "/" + filename);
+            files.transferTo(path);
+            
+            // 써네일 이미지 생성
+            String thumbnailPath = path.getCanonicalPath() + "_50x50";
+            System.out.println(thumbnailPath);
+            Thumbnails.of(path)
+                      .size(50, 50)
+                      .outputFormat("jpg")
+                      .toFile(new File(thumbnailPath));
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return jsonData;
+    }
+
 }
