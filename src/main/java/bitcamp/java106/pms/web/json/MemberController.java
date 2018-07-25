ebe0f851cb265417b5ee211c5e4de2de7ce94d85
Boram Choi <boram3510@gmail.com>
2018-07-25 10:16:39 +0900

    ,

--- src/main/java/bitcamp/java106/pms/web/json/MemberController.java
+++ src/main/java/bitcamp/java106/pms/web/json/MemberController.java
@@ -11,6 +11,7 @@ import javax.servlet.ServletContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
