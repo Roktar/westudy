5cf406d4167c3aaf2301f8330e6c3c6fe3055aa8
janghaeun <cuty_y2@naver.com>
2018-07-26 21:22:26 +0900

    .

--- src/main/java/bitcamp/java106/pms/service/ReviewService.java
+++ src/main/java/bitcamp/java106/pms/service/ReviewService.java
@@ -6,7 +6,6 @@ import java.util.List;
 import bitcamp.java106.pms.domain.Review;
 
 public interface ReviewService {
-    
     Review get(int no);
     int add(Review review);
 	List<Review> selectList(String category);
--- src/main/webapp/js/review2.js
+++ src/main/webapp/js/review2.js
@@ -2,20 +2,19 @@
 
 var list_group = $('.reviewbox');
 
+
 var i = 1;
 
 
 $(document).ready(function() {
-	$.get("json/review/list", {name:'ㄴㄴ'}, res => {
+	$.get("json/review/list", {category:'언어'}, (data) => {
 		let list_group = $('.reviewbox');
 
-		for(let item in res) {
-
-
+		for(var item of data) {
 			$('<li class="list-group-item">' +
-					'<a href="#"><img src="img/'+ res[item].pimg +'"  width="65rem" height="65rem" class="rounded-circle prof-img" alt="logo"></a>'+
+					'<a href="#"><img src="img/'+ item.pimg +'"  width="65rem" height="65rem" class="rounded-circle prof-img" alt="profile"></a>'+
 					'<div id="user_inform">' +
-					'<div id="user_name"><a href="#">'+ res[item].member +'</a></div>'+
+					'<div id="user_name"><a href="#">'+ item.member.name +'</a></div>'+
 					'<div class="user_score">'+
 					'<select id="review-score'+ i +'">'+
 					'<option value="1">1</option>'+
@@ -25,30 +24,36 @@ $(document).ready(function() {
 					'<option value="5">5</option>'+
 					'</select>'+
 					'</div>'+
-					'<div class="user_class">'+ res[item].category +'</div>'+
+					'<div class="user_class">'+ item.study.category +'</div>'+
 					'</div>'+
 					'<form>'+
 					'<div class="form-group">'+
-					'<pre class="form-control review-form" id="user-review" readonly>'+ res[item].content +'</pre>'+
+					'<pre class="form-control review-form" id="user-review" readonly>'+ item.content +'</pre>'+
 					'</div>'+
 					'</form>'+
-					'<span class="review_date">'+ res[item].createdDate +'</span>'+
+					'<span class="review_date">'+ item.createdDate +'</span>'+
 					'<div class="user_class_info">'+
-					'<div class="user_class_name"><a href="groupMain.html">'+ res[item].study +'</a> <span class="study-link">스터디</span></div>'+
-					'<img src="img/'+ res[item].img +'"  class="img-fluid rounded-circle study-prof" alt="logo">'+
+					'<div class="user_class_name"><a href="groupMain.html">'+ item.study.name +'</a> <span class="study-link">스터디</span></div>'+
+					'<img src="img/'+ item.img +'"  class="img-fluid rounded-circle study-prof" alt="study-profile">'+
 					'</div>'+
 					'<hr class="review-line">'+
 			'</li>').appendTo(list_group);
 
-
-
-			<!-- 별점 --> 	
+			/*별점 */ 	
 			$('#review-score' + i).barrating({
 				theme: 'css-stars',
 				readonly: true
 			})
-			$('#review-score' + i).barrating('set', res[item].rating);
+			$('#review-score' + i).barrating('set', item.rating);
 			i++;
 		}
+		console.log(data);
+		let review_count = $('.stdclasstitl2');
+		$('<span class="reviewcount">('+ item.count +')</span>').appendTo(review_count);
+		
 	});
 });
+
+
+
+
