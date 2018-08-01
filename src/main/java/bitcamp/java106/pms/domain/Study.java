f58b7aa91ee01e92665a24acb9b669c5a4c93a0a
janghaeun <cuty_y2@naver.com>
2018-08-01 18:44:26 +0900

    .

--- src/main/java/bitcamp/java106/pms/domain/Study.java
+++ src/main/java/bitcamp/java106/pms/domain/Study.java
@@ -2,6 +2,7 @@ package bitcamp.java106.pms.domain;
 
 import java.io.Serializable;
 import java.sql.Date;
+import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
@@ -12,60 +13,100 @@ public class Study implements Serializable {
     private int no;
     private String city;
     private String county;
-    private String sname;
+    private String name;
     private String information;
-    @JsonFormat(pattern="yyyy-MM-dd", timezone="Asia/Seoul")
+    
+    @JsonFormat(pattern="yyyy-MM-dd")
     private Date createdDate;
+    
     private int maxPeople;
     private String category;
+    
+    private List<Member> members;
+    
+   
+    
+    @Override
+	public String toString() {
+		return "StudyInfoTest [no=" + no + ", city=" + city + ", county=" + county + ", name=" + name + ", information="
+				+ information + ", createdDate=" + createdDate + ", maxPeople=" + maxPeople + ", category=" + category
+				+ ", members=" + members + "]";
+	}
 
-    public int getNo() {
+    
+	public int getNo() {
         return no;
     }
+
     public void setNo(int no) {
         this.no = no;
     }
+
     public String getCity() {
         return city;
     }
+
     public void setCity(String city) {
         this.city = city;
     }
+
     public String getCounty() {
         return county;
     }
+
     public void setCounty(String county) {
         this.county = county;
     }
+
     public String getName() {
-        return sname;
+        return name;
     }
+
     public void setName(String name) {
-        this.sname = name;
+        this.name = name;
     }
+
     public String getInformation() {
         return information;
     }
+
     public void setInformation(String information) {
         this.information = information;
     }
+
     public Date getCreatedDate() {
         return createdDate;
     }
+
     public void setCreatedDate(Date createdDate) {
         this.createdDate = createdDate;
     }
+
     public int getMaxPeople() {
         return maxPeople;
     }
+
     public void setMaxPeople(int maxPeople) {
         this.maxPeople = maxPeople;
     }
+
     public String getCategory() {
         return category;
     }
+
     public void setCategory(String category) {
         this.category = category;
     }
+
+
+	public List<Member> getMembers() {
+		return members;
+	}
+
+
+	public void setMembers(List<Member> members) {
+		this.members = members;
+	}
+    
     
 }
--- src/main/java/bitcamp/java106/pms/service/StudyService.java
+++ src/main/java/bitcamp/java106/pms/service/StudyService.java
@@ -2,12 +2,17 @@ package bitcamp.java106.pms.service;
 
 import java.util.List;
 
+import bitcamp.java106.pms.domain.HashTag;
 import bitcamp.java106.pms.domain.Study;
+import bitcamp.java106.pms.domain.StudyInfo;
 
 public interface StudyService {
-    List<Study> list();
+    List<Study> list(int pageNo, int pageSize);
+    List<HashTag> listTag(int no);
     Study get(int no);
     int add(Study study);
-    int update(Study study);
+    int addTag(String[] tag);
+    int update(Study studyInfo);
     int delete(int no);
+    int getLimitOne();
 }
