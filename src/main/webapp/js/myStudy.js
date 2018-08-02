c5ad0385daac96186c3180f7ad3ed03f30e7c098
janghaeun <cuty_y2@naver.com>
2018-08-02 11:51:16 +0900

    .

--- src/main/webapp/js/myStudy.js
+++ src/main/webapp/js/myStudy.js
@@ -1,22 +1,22 @@
-
-  
- var list_group = $('.mystudy');
- 
-
  $(document).ready(function() {
-	 $.get("json/review/list", {}, (data) => {
+	 
+	 
+	 
+	 $.get("json/review/listall", {}, (data) => {
+		 let list_group = $('.mystudy');
+		 
 
-		for(var item of rv) {
+		for(var item of data) {
 			console.log(item)
 			$('<div>'+
-            '<span class="gg">'+ item.category +' 스터디 </span><a href="groupMain.html">\''+ item.study +'\'</a>'+
+            '<span class="gg">'+ item.study.category +' 스터디 </span><a href="groupMain.html">\''+ item.study.name +'\'</a>'+
             '<button class="btn btn-secondary btn-sm ttbtn" data-toggle="modal" data-target="#tmodal">탈퇴</button>'+
             '<a data-toggle="modal" href="#dAD"><button class="btn btn-primary btn-sm review">후기 작성</button></a>'+
             '</div>').appendTo(list_group);
 			
 		}
-	 }
- }
+	});
+ });
 	
 
 
