6a9b21b10a2eeba28e55b2cf8f3f3c19b20ea1ab
janghaeun <cuty_y2@naver.com>
2018-08-02 16:33:03 +0900

    .

--- src/main/java/bitcamp/java106/pms/web/json/StudyJoinedMemberController.java
+++ src/main/java/bitcamp/java106/pms/web/json/StudyJoinedMemberController.java
@@ -5,7 +5,7 @@ import javax.servlet.ServletContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestParam; 
 import org.springframework.web.bind.annotation.RestController;
 
 import bitcamp.java106.pms.domain.StudyJoinedMember;
