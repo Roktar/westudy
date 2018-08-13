054945c608e0cb6359c99901109f479a8bd7bd60
janghaeun <cuty_y2@naver.com>
2018-08-13 21:37:36 +0900

    .

--- src/main/webapp/js/mainReview.js
+++ src/main/webapp/js/mainReview.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 	$.get("json/review/listall" , {}, data =>  {
 		let list_group = $('.reviewBox');
-		
+		console.log(data);
 		for(var item of data) {
 					$('<li class="l-col">'+
                         '<div class="study">'+
@@ -12,7 +12,7 @@ $(document).ready(function() {
                             '<h2>'+ item.study.name +'</h2>'+
                             '<p>'+ item.content +'</p>'+
                             '<div class="study-hover">'+
-                                '<a href="http://localhost:8888/FinalProject/groupMain.html"><span>스터디 둘러보기</span></a>'+
+                                '<a href="groupMain.html?no='+ item.studyNo +'"><span>스터디 둘러보기</span></a>'+
                             '</div>'+
                         '</div>'+
                     '</li>').appendTo(list_group);
