c0a76d2d732119db3a5d97c216dcdf6e82695ec7
janghaeun <cuty_y2@naver.com>
2018-08-13 21:33:46 +0900

    .

--- src/main/webapp/js/groupMain.js
+++ src/main/webapp/js/groupMain.js
@@ -3,17 +3,75 @@ var studyName = document.querySelectorAll(".studyName");
 var stdno = location.href.split("?")[1].split("=")[1];
 
 $(document).ready(function() {
+	
+	
 	$(".groupInfoUpdate").click(function(){
 		location.href =  "groupInfoUpdate.html?no="+stdno;
 	});
 	
     $.get("json/auth/loginstat", {}, res=> {
-    	memno = res.no;
+    	var memno = res.no;
     }).done(function(data){
+    	/* 스터디 정보 */
     	$.getJSON("json/studyInfo/" + stdno, function(data) {
             $(studyName).text(data.name);
             $(fInformation).text(data.information);
+            $('.maxPeople').text(data.maxPeople);
         }); 
+    	
+    	/* 리더정보 가져오기 */
+    	 $.getJSON("json/joinedMember/leaderList", {no: stdno}, data =>{
+      		  console.log(data);
+      		 console.log(data.member.name);
+      		$('.leaderName').text(data.member.name);
+      		$(leaderpfp).attr('src', 'files/' + data.member.photo + '_350x350.jpg');
+          });
+    	 
+    	 
+    	  /* 멤버정보가져오기 */
+         $.getJSON("json/joinedMember/listMember", {no: stdno}, data =>{
+      		  console.log(data);
+      		  let memberInfo = $('.memberInfo');
+      		  
+      		  for (var item of data) {
+      			  $('<div class="col-4">'+
+                    '<div class="row eachMember">'+
+                       '<div class="col-sm">'+
+                            '<div class="member-img"><img src="files/'+ item.member.photo +'_350x350.jpg" alt="profile"></div>'+
+                        '</div>'+
+                        '<div class="col-sm memberProfile">'+
+                            (item.grade == 1 ?'<span class="memberClass">회원</span>' : '<span class="memberClass">리더</span>')+
+                            '<span class="memberName">'+ item.member.name +'</span>'+
+                        '</div>'+
+                        '<div class="col-sm message">'+
+                            '<a href="message.html">'+
+                                '<img src="img/message.png" alt="..." class="message" onclick="">'+
+                            '</a>'+
+                        '</div>'+
+                    '</div>'+
+                '</div>').appendTo(memberInfo);
+      		  }
+      		
+          }); 
+         
+         
+         /* 멤버 카운트 */
+         $.getJSON("json/joinedMember/count", {no : stdno}, data => {
+      	   		$('.memberCount').text(data);
+      	   		
+         });
+
+         /* 타임라인 */
+         $.getJSON("json/schedule/listdetail/" + stdno, (data) => {
+      	   console.log(data);
+ 	   		$('.scheduleLocationDetail').text(data[0].placeDetail);
+ 	   		$('.scheduleTopics').text(data[0].title);
+ 	   	    $('.scheduleTimelineDetail').text(data[0].content);
+    });
+          
+    	 
+    	 
+    	 
     });
     // 가입요청하기 기능
     $(".acceptRequest").click(function(memno, stdno){
